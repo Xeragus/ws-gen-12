f2fd90509d3ed8d544fa9d6dc32a791fced8309d
Boban Sugareski <boban.s@slicelife.com>
2021-04-14 22:00:26 +0200

    Add user model, fetch all and create routes

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-app.get('/hehe', (req, res) => {
-  res.send({
-    message: 'dojdovte na GET /hehe',
-    hehe: true,
-    haha: 1234,
-    hihi: [1, 2, 3, 4]
-  });
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/ws-gen-12', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 });
+
+app 
+  .get('/users', async (req, res) => {
+    const users = await User.find();
+
+    res.send({
+      error: false,
+      message: 'List of all available users',
+      users: users
+    })
+  })
+  .post('/users', async (req, res) => {
+    const user = await User.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      message: `Uspesno zacuvan korisnik so ime ${user.full_name} i email ${user.email}`
+    });
+  });
 
 app.listen(3000, () => {
   console.log('App is started on port 3000...');
 })
-
-// <--------------------response------------------- server

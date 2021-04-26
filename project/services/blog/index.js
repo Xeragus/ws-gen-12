993a6b3d7bcf9ba0c6a0016ae926ce7f122e39e7
Boban Sugareski <boban.s@slicelife.com>
2021-04-26 20:12:22 +0200

    Add initial structure for project

---
+++ project/services/blog/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const blogPostsRouter = require("./routes/blogposts");
+const categoriesRouter = require("./routes/categories");
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost/ws-gen-12", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use("/blogposts", blogPostsRouter);
+app.use("/categories", categoriesRouter);
+
+app.listen(3000, () => {
+  console.log("App is started on port 3000...");
+});
